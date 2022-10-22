d8807094882419f5b5ab9047f015521bef6de4c4
Your Name <you@example.com>
2022-10-22 15:46:00 +0800

    0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,23 @@ mod account {
     // pub use super::*;
     mod auth {
         pub type AccountId = u64;
+        pub type OpenId = u64;
+        pub type UnionId = u64;
+        pub type AppId = u64;
+        pub type TenantFlowId = u64;
+        pub type TenantKey = u64;
     }
     pub use auth::*;
+    mod token {
+        pub type UserAccessToken = String;
+        pub type TenantAccessToken = String;
+        pub type AppAccessToken = String;
+
+        pub type TicketValue = u64;
+    }
+    pub use token::*;
     mod trading {
-        use crate::AccountId;
+        use crate::*;
 
         pub type PaymentId = u64;
         pub type CustomerId = AccountId;
@@ -17,6 +30,7 @@ mod account {
     pub use trading::*;
 
     pub type Name = String;
+    pub type AppPermissions = u64;
     // pub type
 }
 // pub use account::{auth::*, trading::*, AccountId, Name};
@@ -31,6 +45,16 @@ mod thing {
     pub type UserId = OwnerId;
     pub type TeamId = OwnerId;
     pub type Picture = Vec<u8>;
+    mod chat {
+        pub type ChatId = u64;
+        pub type OpenMessageId = u64;
+    }
+    pub use chat::*;
+    mod department {
+        pub type OpenDepartmentId = u64;
+        pub type DepartmentId = u64;
+    }
+    pub use department::*;
 }
 pub use thing::*;
 mod place {
